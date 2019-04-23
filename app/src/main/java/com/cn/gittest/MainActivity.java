b883b6b32952eb0639e4843766c296ee9882ec47
Jhon354202 <2230859583@qq.com>
2019-04-23 14:58:50 +0800

    First commit.

---
+++ app/src/main/java/com/cn/gittest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cn.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//https://github.com/Jhon354202/gitTest.git
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
